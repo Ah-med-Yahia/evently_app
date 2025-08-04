58cd9c622eca5a9581b694dc633f8991f2212bd9
Ahmed Yahia <ahmed22y10@gmail.com>
2025-08-04 14:29:34 +0200

    initialize project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:evently_app/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EventlyApp());
+}
+
+class EventlyApp extends StatelessWidget {
+  const EventlyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      routes: {
+        HomeScreen.routeName:(_)=> const HomeScreen()
+      },
+    );
+  }
+}
