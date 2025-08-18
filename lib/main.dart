66f869e47db62244e92bccbf6861e70d3efea488
Ahmed Yahia <ahmed22y10@gmail.com>
2025-08-18 11:31:05 +0200

    Integrate flutter with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'dart:async';
+
 import 'package:evently_app/auth/login_screen.dart';
 import 'package:evently_app/auth/register_screen.dart';
+import 'package:evently_app/firebase_options.dart';
 import 'package:evently_app/screens/create_event_screen.dart';
 import 'package:evently_app/screens/home_screen.dart';
 import 'package:evently_app/utils/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const EventlyApp());
 }
 
